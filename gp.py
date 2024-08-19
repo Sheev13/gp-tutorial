9a174e66a59cde9e76ce5d6a3c06773aa717c6a0
Tommy <rochussentn@gmail.com>
2024-08-19 10:30:08 +0100

    learning sigma_y mp4

--- gp.py
+++ gp.py
@@ -19,13 +19,15 @@ def gp_prior(X: torch.Tensor, cov: str = 'se', l: float = 1.0):
     K = covariance_matrix(X, X, cov=cov, l=l) + torch.eye(X.shape[0])*1e-8
     return torch.distributions.MultivariateNormal(loc=torch.zeros_like(X), covariance_matrix=K)
 
-def gp_posterior(X: torch.Tensor, y: torch.Tensor, x_test: torch.Tensor, cov: str = 'se', sigma_y: float = 0.0, l: float = 1.0):
+def gp_posterior(X: torch.Tensor, y: torch.Tensor, x_test: torch.Tensor, cov: str = 'se', sigma_y: float = 0.0, l: float = 1.0, observation_noise: bool = False):
     K_nn = covariance_matrix(X, X, cov=cov, l=l) + (sigma_y**2 + 1e-8)*torch.eye(X.shape[0])
     chol = torch.linalg.cholesky(K_nn)
     inv = torch.cholesky_inverse(chol)
 
     K_tn = covariance_matrix(x_test, X, cov=cov, l=l)
-    K_tt = covariance_matrix(x_test, x_test, cov=cov, l=l) + (1e-8)*torch.eye(x_test.shape[0])  # add sigma_y here for y_* predictive (as opposed to f_*)
+    K_tt = covariance_matrix(x_test, x_test, cov=cov, l=l) + (1e-8)*torch.eye(x_test.shape[0]) 
+    if observation_noise: # True if p(y_*|...), False if p(f_*|...)
+        K_tt += sigma_y**2 * torch.eye(x_test.shape[0])
 
     mu = K_tn @ inv @ y
     covar = K_tt - (K_tn @ inv @ K_tn.T)
@@ -42,6 +44,7 @@ def get_samples(gp: torch.distributions.MultivariateNormal, n: int = 10):
         samps[i,:] = gp.sample()
     return samps
 
+# The below class is for (visualisations to do with) training GPs.
 class GP(nn.Module):
     def __init__(self, sigma_y: float = 0.0, learn_sigma_y: bool = False, l: float=1.0, learn_l: bool=False):
         super().__init__()
@@ -56,12 +59,12 @@ class GP(nn.Module):
     def l(self):
         return self.log_l.exp()
 
-    def forward(self, x_test: torch.Tensor, X: Optional[torch.Tensor] = None, y: Optional[torch.Tensor] = None, cov: str ='se'):
+    def forward(self, x_test: torch.Tensor, X: Optional[torch.Tensor] = None, y: Optional[torch.Tensor] = None, cov: str ='se', observation_noise: bool = False):
         if X is None:
             return gp_prior(x_test, cov=cov, l=self.l)
         else:
             assert y is not None
-            return gp_posterior(X, y, x_test, cov=cov, l=self.l, sigma_y=self.sigma_y)
+            return gp_posterior(X, y, x_test, cov=cov, l=self.l, sigma_y=self.sigma_y, observation_noise=observation_noise)
         
     def log_marginal_likelihood(self, X: torch.Tensor, y: torch.Tensor, cov: str = 'se'):
         I = torch.eye(X.shape[0])
